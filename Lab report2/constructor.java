2090ca445f5b072ddf0587b852ad2f6d6baa9cc9
jabed366 <jabedhasan231@gmail.com>
2022-10-26 22:25:59 +0600

    Constructor

---
+++ Lab report2/constructor.java
@@ -0,0 +1,26 @@
+
+class Student{
+    String Name;
+    int id;
+    public Student(String Name,int roll){
+        this.Name=Name;
+        this.id=roll;
+    }
+    public Student(int roll,String name){
+        this.Name=name;
+        this.id=roll;
+    }
+    public void display(){
+        System.out.println(Name);
+        System.out.println(id);
+    }
+}
+
+public class constructor{
+    public static void main(String[] args){
+       Student s1=new Student("Jabed", 0);
+       Student s2=new Student(10,"Nayeem");
+        s1.display();
+        s2.display();
+    }
+}

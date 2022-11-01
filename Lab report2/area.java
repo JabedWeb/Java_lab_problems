d79b317e443f9ef97c415ac5478cfb6843843144
jabed366 <jabedhasan231@gmail.com>
2022-11-01 21:51:34 +0600

    Constructor

--- Lab report2/area.java
+++ Lab report2/area.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 /*
@@ -15,57 +14,56 @@ class NumberValue {
     int height;
     String areaname;
     double area;
-  public NumberValue(int l,int h,String type) {
-      length=l;
-      height=h;
-      areaname=type;
-      area=0.5*length*height;
-  }
-   public NumberValue(String type,int h,int l) {
-      length=l;
-      height=h;
-      areaname=type;
-      area=length*height;
-  }
-      public NumberValue(String type,int r) {
-      length=r;
-      areaname=type;
-      area=3.14*(r*r);
-  }
-  public void display() {
-     System.out.println("total area of "+ areaname +" "+ area);
-  }
+    public NumberValue(int l, int h, String type) {
+        length = l;
+        height = h;
+        areaname = type;
+        area = 0.5 * length * height;
+    }
+    public NumberValue(String type, int h, int l) {
+        length = l;
+        height = h;
+        areaname = type;
+        area = length * height;
+    }
+    public NumberValue(String type, int r) {
+        length = r;
+        areaname = type;
+        area = 3.14 * (r * r);
+    }
+    public void display() {
+        System.out.println("total area of " + areaname + " " + area);
+    }
 }
 public class area {
-  public static void main(String[] args) {
-      Scanner scan=new Scanner(System.in);
-      System.out.print("1 for tri \n 2 for rec \n  for circle\n");
-      int select=scan.nextInt();
-      
-      switch (select) {
-          case 1:
-              System.out.print("Please input the length and height\n");
-              int length=scan.nextInt();
-              int height=scan.nextInt();
-               NumberValue tri = new NumberValue(length,height,"tri");
-               tri.display();
-              break;
-               case 2:
-                   System.out.print("Please input the length and height\n");
-                   int lengths=scan.nextInt();
-              int heights=scan.nextInt();
-                NumberValue rec = new NumberValue("rec",heights,lengths);
-               rec.display();
-              break;
-                case 3:
-                    System.out.print("Please input circle area\n");
-                    int circles=scan.nextInt();
-                NumberValue circle = new NumberValue("circle",circles);
-               circle.display();
-              break;
-          default:
-              throw new AssertionError();
-      }
-  }
-}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("1 for tri \n 2 for rec \n  for circle\n");
+        int select = scan.nextInt();
 
+        switch (select) {
+            case 1:
+                System.out.print("Please input the length and height\n");
+                int length = scan.nextInt();
+                int height = scan.nextInt();
+                NumberValue tri = new NumberValue(length, height, "tri");
+                tri.display();
+                break;
+            case 2:
+                System.out.print("Please input the length and height\n");
+                int lengths = scan.nextInt();
+                int heights = scan.nextInt();
+                NumberValue rec = new NumberValue("rec", heights, lengths);
+                rec.display();
+                break;
+            case 3:
+                System.out.print("Please input circle area\n");
+                int circles = scan.nextInt();
+                NumberValue circle = new NumberValue("circle", circles);
+                circle.display();
+                break;
+            default:
+                throw new AssertionError();
+        }
+    }
+}

3928da9f95918f18afe445b2d388f8b1488163ed
jabed366 <jabedhasan231@gmail.com>
2022-11-03 17:45:04 +0600

    Java File Read,write,delete,create

---
+++ Lab Report3/file.java
@@ -0,0 +1,73 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+
+public class file {
+    public static void main(String[] args) {
+
+        //directory creation
+        File dr=new File("D:/Java/Lab Report3/student");
+        String path=dr.getAbsolutePath();
+        try {
+            if(dr.exists()) {
+                System.out.println("Directory already exists");
+            }
+            else {
+                dr.mkdir();
+                System.out.println("Directory created");
+            }
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+
+        //file creation
+        System.out.println(path);
+
+        File file=new File(path+"/student.txt");
+
+        try {
+            if(file.exists()) {
+                System.out.println("File already exists");
+            }
+            else {
+                file.createNewFile();
+                System.out.println("File created");
+            }
+        } catch (Exception e) {
+            System.out.print(e);
+        }
+
+        //file writing
+        try{
+            FileWriter fileWriter = new FileWriter(path+"/student.txt"); 
+            fileWriter.write("Name: Jabed\n");
+            fileWriter.write("Name: Hasan\n");
+            fileWriter.write("Name: Fahim\n");
+            fileWriter.close();
+        } catch(Exception e) {
+            System.out.print(e);
+        }
+
+
+        //reading file
+
+        try {
+            File files=new File(path+"/student.txt");
+            Scanner sc=new Scanner(files);
+            while(sc.hasNext()) {
+                System.out.print("\n"+sc.nextLine());
+
+            }
+            sc.close();
+        } catch (Exception e) {
+           System.out.print(e);
+        }
+
+        
+
+
+        
+
+}
+}

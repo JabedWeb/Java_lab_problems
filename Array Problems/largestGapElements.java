76d9bf7ff323d06f12d463dc11bef0cc7fb9eb38
jabed366 <jabedhasan231@gmail.com>
2022-11-05 18:11:10 +0600

    largestElements.Gap

---
+++ Array Problems/largestGapElements.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class largestGapElements {
+    public static void main(String[] args) {
+        System.out.print("Enter the size of the array : ");
+        Scanner scan=new Scanner(System.in);
+        int array_size=scan.nextInt();
+        int [] array =new int[array_size];
+        for(int i=0;i<array_size;i++){
+            System.out.print("Enter the element at index "+i+" : ");
+            array[i]=scan.nextInt();
+        }
+        Arrays.sort(array);
+        int max=array[0];
+        for(int i=1;i<array.length;i++){
+            if(array[i]-array[i-1]>max){
+                max=array[i]-array[i-1];
+            }
+        }
+        System.out.println("The largest gap between two elements is "+max);   
+    }
+}

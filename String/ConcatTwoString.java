1e3a008d6cafcf5b256a2352d7f80373d1d6776f
jabed366 <jabedhasan231@gmail.com>
2022-11-06 12:19:05 +0600

    two String concate

---
+++ String/ConcatTwoString.java
@@ -0,0 +1,10 @@
+package String;
+
+public class ConcatTwoString {
+    public static void main(String[] args) {
+        String str1="Hello";
+        String str2="World";
+        String str3=str1.concat(str2);
+        System.out.println(str3);
+    }
+}
---
+++ String/compareTwoString.java
@@ -0,0 +1,16 @@
+package String;
+
+public class compareTwoString {
+    public static void main(String[] args) {
+        String str1="Helloo";
+        String str2="Hello";
+        int com=str1.compareTo(str2);
+       if(com==0){
+           System.out.println("\"" + str1 + "\"" +"Both the string are equal");
+       }
+       else{
+           System.out.println("Both the string are not equal");
+       }
+    }
+    
+}

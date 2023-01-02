73ee38c9b0feaec0fd6f1a58ac94f0a1d6960ca5
jabed366 <jabedhasan231@gmail.com>
2023-01-02 18:45:47 +0600

    Price

---
+++ String/Stringjava.java
@@ -0,0 +1,15 @@
+package String;
+
+public class Stringjava {
+
+   public static void main(String[] args) {
+String name = "Nayem"; 
+
+StringBuilder name1=new StringBuilder();
+name1.append(name);
+  System.out.println(name1.reverse());
+
+    for(name1 : )
+
+   }
+}
